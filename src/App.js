b76ce206cb4d2688efc3b2d777aaa3f1d5c6a820
attiliogiglio <attilio.g93@gmail.com>
2020-09-15 19:18:49 -0300

    request and reading data API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './Components/Header';
 import Form from './Components/Form';
 
@@ -7,6 +7,18 @@ function App() {
   // definir la categoria y noticias
 
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  const consultarAPI = async() => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=de35d64039c44798a8e66bb93aaae684` 
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+    guardarNoticias(noticias.articles);
+  }
+
+  useEffect(() => {
+    consultarAPI();
+  }, [categoria])
 
   return (
     <Fragment>
