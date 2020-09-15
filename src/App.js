684b12efb735b50b5b7d4c00de28e8912b7a87a5
attiliogiglio <attilio.g93@gmail.com>
2020-09-15 18:17:06 -0300

    adding header and main container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Fragment>
+      <Header titulo='Buscador de noticias' />
+    </Fragment>
   );
 }
 
